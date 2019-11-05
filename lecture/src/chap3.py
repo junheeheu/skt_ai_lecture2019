2553f9d7c589093c2836d032217df4227b9e6650
영상인식기술팀 허준희 <junhee.heu@sk.com>
2019-11-05 16:27:04 +0900

    Using SKT Face Recognition API

--- lecture/src/chap3.py
+++ lecture/src/chap3.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-@file chap2.py
-@brief create name space
+@file chap3.py
+@brief register faces
 @author Jun-Hee HEU
 @contact junhee.heu@sk.com
 @data 2019.11.05
 """
 
 from api_util import sktface
+import os
 
 print('--------- Step. 2. Register your face in your namespace ---------')
 
 imgroot = '../../../enrol'
 
 skt = sktface(name)
-skt.create_name()
+
+imglist = os.listdir(imgroot)
+
+for imgname in imglist:
+    imgpath = '%s/%s' % (imgroot, imgname)
+    skt.register_face(imgpath)
 
 print('--------- Go To Next Chapter ---------')
