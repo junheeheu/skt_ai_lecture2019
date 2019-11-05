d8ea6259c14cfe524bfd1e8dd7930e0dd238b671
영상인식기술팀 허준희 <junhee.heu@sk.com>
2019-11-05 17:31:57 +0900

    Using SKT Face Recognition API

--- lecture/src/chap4.py
+++ lecture/src/chap4.py
@@ -9,6 +9,7 @@
 
 from api_util import sktface
 import os
+import cv2
 
 print('--------- Step. 3. Identify a face ---------')
 
@@ -24,7 +25,15 @@ for imgname in imglist:
     if not os.path.exists(imgpath):
         print('check the path')
     results = skt.identify(imgpath)
-    import pdb; pdb.set_trace()
-    temp = 0
+    img = cv2.imread('%s/%s' % (imgroot, imgname))
+    sx = results['face_box']['topLeftX']
+    sy = results['face_box']['topLeftY']
+    ex = sx + results['face_box']['faceWidth']
+    ey = sy + results['face_box']['faceHeight']
+    cv2.rectangle(img, (sx,sy), (ex,ey), (0,255,255), 5)
+    txt = 'name: %s, %s, %d, %s' % (results['subject_name'], results['gender'], results['age'], results['expression'])
+    cv2.putText(img, txt, (sx + 10, sy+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 1)
+
+    cv2.imwrite('testresult.jpg', img)
 
 print('--------- Go To Next Chapter ---------')
